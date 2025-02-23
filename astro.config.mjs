c7ce2e9ac5d7c8236c6a297bb7364e592195f8cd
Lyn <0xlynett@proton.me>
2025-02-23 21:57:02 +0000

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    icon(),
-  ],
+  integrations: [react(), icon()],
+  vite: {
+    plugins: [
+      tailwind({
+        applyBaseStyles: false,
+      }),
+    ],
+  },
 });
